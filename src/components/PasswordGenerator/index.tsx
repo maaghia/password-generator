bc86a237b6e18799e423010be2bee1effa60f21d
maaghia <toubalmaria23@gmail.com>
2023-08-04 23:03:37 +0100

    password length slider refresh

--- src/components/PasswordGenerator/index.tsx
+++ src/components/PasswordGenerator/index.tsx
@@ -1,18 +1,65 @@
 import Slider from 'rc-slider'
 import 'rc-slider/assets/index.css'
-import { useState } from 'react'
-
+import { SetStateAction, useState } from 'react'
 import passwordGif from '../../assets/gif/password.gif'
 import { ReactComponent as Copy } from '../../assets/icons/copy.svg'
 import { ReactComponent as Refresh } from '../../assets/icons/refresh.svg'
 import Checkbox from '../Checkbox'
 import './index.css'
 
+
 const PasswordGenerator = () => {
-  const [passwordLength, setPasswordLength] = useState<number>(8)
+  const [passwordLength, setPasswordLength] = useState<number>(8);
+  const [randomPassword, setRandomPassword] = useState('46QBKCVP');
+
+  /* const handlePasswordChange = (event: { target: { value: SetStateAction<string> } }) => {
+    setRandomPassword(event.target.value);
+  }; */
+  
+  const handleRefresh = () => {
+    const newPassword = generateRandomPassword(passwordLength);
+    setRandomPassword(newPassword);
+  };
 
   const onChangePasswordLength = (value: number | number[]) => {
     setPasswordLength(value as number)
+  }
+  
+  const getRandomCharFromArray = (array: string | any[]) => {
+    let randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+  }
+
+  const generateRandomPassword = (length: number | number[]) => {
+    const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+    const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numbers = '0123456789';
+    const specialCharacters = '!@#$%^&*()_+~`|}{[]\\:;?><,./-=';
+
+    let randomPassword = '';
+
+    for (let i = 0; i < length; i++) {
+      const randomChoice = Math.floor(Math.random() * 4); 
+
+      switch (randomChoice) {
+        case 0:
+          randomPassword += getRandomCharFromArray(lowercaseLetters);
+          break;
+        case 1:
+          randomPassword += getRandomCharFromArray(uppercaseLetters);
+          break;
+        case 2:
+          randomPassword += getRandomCharFromArray(numbers);
+          break;
+        case 3:
+          randomPassword += getRandomCharFromArray(specialCharacters);
+          break;
+        default:
+          break;
+      }
+    }
+
+    return randomPassword;
   }
 
   return (
@@ -28,8 +75,10 @@ const PasswordGenerator = () => {
       </div>
       <div className="password-input-wrapper">
         <div className="password-field">
-          <input type="text" placeholder="your password" value="B9QI4PDBYY" />
+          <input type="text" placeholder="your password" value={randomPassword} readOnly/>
+            <button onClick={handleRefresh}>
           <Refresh />
+          </button>
         </div>
         <button className="copy-btn">
           <Copy /> Copy
