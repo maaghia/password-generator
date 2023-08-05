4f8db70cfb9881d901316b1eb5ed87e9a8027e1a
maaghia <toubalmaria23@gmail.com>
2023-08-05 13:48:39 +0100

    checkbox & generate password

--- src/components/PasswordGenerator/index.tsx
+++ src/components/PasswordGenerator/index.tsx
@@ -11,11 +11,15 @@ import './index.css'
 const PasswordGenerator = () => {
   const [passwordLength, setPasswordLength] = useState<number>(8);
   const [randomPassword, setRandomPassword] = useState('46QBKCVP');
+  const [uppercaseChecked, setUppercaseChecked] = useState(true);
+  const [lowercaseChecked, setLowercaseChecked] = useState(false);
+  const [numbersChecked, setNumbersChecked] = useState(false);
+  const [specialCharsChecked, setSpecialCharsChecked] = useState(true);
 
-  /* const handlePasswordChange = (event: { target: { value: SetStateAction<string> } }) => {
-    setRandomPassword(event.target.value);
-  }; */
-  
+  console.log("first", uppercaseChecked, lowercaseChecked, numbersChecked, specialCharsChecked);
+  if (!uppercaseChecked && !lowercaseChecked && !numbersChecked && !specialCharsChecked) {
+    setLowercaseChecked(!lowercaseChecked);
+  } 
   const handleRefresh = () => {
     const newPassword = generateRandomPassword(passwordLength);
     setRandomPassword(newPassword);
@@ -36,22 +40,40 @@ const PasswordGenerator = () => {
     const numbers = '0123456789';
     const specialCharacters = '!@#$%^&*()_+~`|}{[]\\:;?><,./-=';
 
+    let allChar = '';
+    if(uppercaseChecked) allChar += uppercaseLetters;
+    if(lowercaseChecked) allChar += lowercaseLetters;
+    if(numbersChecked) allChar += numbers;
+    if(specialCharsChecked) allChar += specialCharacters;
+
+    let switchIndex = '';
+
+    if(lowercaseChecked) switchIndex += '0';
+    if(uppercaseChecked) switchIndex += '1';
+    if(numbersChecked) switchIndex += '2';
+    if(specialCharsChecked) switchIndex += '3';
+    
+    console.log(switchIndex);
+
     let randomPassword = '';
 
     for (let i = 0; i < length; i++) {
-      const randomChoice = Math.floor(Math.random() * 4); 
+      const randomChoice = getRandomCharFromArray(switchIndex); 
+      
+
+      console.log('----'+randomChoice)
 
       switch (randomChoice) {
-        case 0:
+        case '0':
           randomPassword += getRandomCharFromArray(lowercaseLetters);
           break;
-        case 1:
+        case '1':
           randomPassword += getRandomCharFromArray(uppercaseLetters);
           break;
-        case 2:
+        case '2':
           randomPassword += getRandomCharFromArray(numbers);
           break;
-        case 3:
+        case '3':
           randomPassword += getRandomCharFromArray(specialCharacters);
           break;
         default:
@@ -61,6 +83,25 @@ const PasswordGenerator = () => {
 
     return randomPassword;
   }
+
+  // Event handlers for checkbox state updates
+  const handleUppercaseChange = () => {
+    setUppercaseChecked(!uppercaseChecked);
+  };
+
+  const handleLowercaseChange = () => {
+    setLowercaseChecked(!lowercaseChecked);
+    console.log(uppercaseChecked, lowercaseChecked, numbersChecked, specialCharsChecked);
+  };
+
+  const handleNumbersChange = () => {
+    setNumbersChecked(!numbersChecked);
+    console.log(uppercaseChecked, lowercaseChecked, numbersChecked, specialCharsChecked);
+  };
+
+  const handleSpecialCharsChange = () => {
+    setSpecialCharsChecked(!specialCharsChecked);
+  };
 
   return (
     <div className="password-wrapper">
@@ -76,7 +117,7 @@ const PasswordGenerator = () => {
       <div className="password-input-wrapper">
         <div className="password-field">
           <input type="text" placeholder="your password" value={randomPassword} readOnly/>
-            <button onClick={handleRefresh}>
+          <button onClick={handleRefresh}>
           <Refresh />
           </button>
         </div>
@@ -99,15 +140,10 @@ const PasswordGenerator = () => {
         />
       </div>
       <div className="elements">
-        <Checkbox id="uppercase" label="Uppercase" checked={true} name="upper" />
-        <Checkbox id="lowercase" label="Lowercase" checked={false} name="lower" />
-        <Checkbox id="numbers" label="Numbers" checked={false} name="numbers" />
-        <Checkbox
-          id="special chars"
-          label="Special Characters"
-          checked={true}
-          name="specialChars"
-        />
+        <Checkbox id="uppercase" label="Uppercase" checked={uppercaseChecked} onChange={handleUppercaseChange} name="upper" />
+        <Checkbox id="lowercase" label="Lowercase" checked={lowercaseChecked} onChange={handleLowercaseChange} name="lower" />
+        <Checkbox id="numbers" label="Numbers" checked={numbersChecked} onChange={handleNumbersChange} name="numbers" />
+        <Checkbox id="special chars" label="Special Characters" checked={specialCharsChecked} onChange={handleSpecialCharsChange} name="specialChars"/>
       </div>
     </div>
   )
